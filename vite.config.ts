5b80eaa63947205d9d7b484f2e0a7f7f644f1cc9
Charles Javelona <charlesjavelona@Charless-MacBook-Pro.local>
2024-02-14 09:47:56 -0500

    Removed resolver

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/valenties",
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
+  base: "/valenties/",
 });
